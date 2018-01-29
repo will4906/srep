aee0443d16c44f7468ecc347fc4696949d0c48d2
will4906 <553105821@qq.com>
2018-01-29 21:31:50 +0800

    change cut data

--- cut_data.py
+++ cut_data.py
@@ -32,85 +32,71 @@ def add_two_rows():
 
 def add_a_period():
     '''
-    1, 2, ..., 15, 16, 1, 2, 3, 4
-    16, 1, 2, ..., 15, 16, 1, 2, 3
-    15, 16, 1, 2, ..., 15, 16, 1, 2
-    14, 15, 16, 1, 2, ..., 15, 16, 1
-    13, ..., 16, 1, 2, ..., 15, 16
-    12, ..., 16, 1, 2, ..., 15
-
-    15, 16, 1, 2, ..., 15, 16, 1, 2
-    14, 15, 16, 1, 2, ..., 15, 16, 1
-    13, 14, 15, 16, 1, 2, ..., 15, 16
-    12, 13, 14, 15, 16, 1, 2, ..., 15
-    11, ..., 16, 1, 2, ..., 14
-    10, ..., 16, 1, 2, ..., 13
-    9, ..., 16, 1, 2, ..., 12
-    8, ..., 16, 1, 2, ..., 11
-    7, ..., 16, 1, 2, ..., 10
-    6, ..., 16, 1, 2, ..., 9
-    5, ..., 16, 1, 2, ..., 8
-    4, ..., 16, 1, 2, ..., 7
-    3, ..., 16, 1, 2, ..., 6
-    2, ..., 16, 1, 2, ..., 5
-    1, ..., 16, 1, 2, ..., 4
-    16, 1, 2, ..., 16, 1, 2, 3
+    cut data to odd number trial with even numbers rotate
+    cut data to even number trial with odd number rotate
     :return:
     '''
     data_path = os.path.join('.cache', 'dba', 'data')
     mat_list = os.listdir(data_path)
     for mat_name in mat_list:
+        trial_index = int(mat_name.split('.')[0].split('-')[-1])
         mat_path = os.path.join(data_path, mat_name)
         mat = scipy.io.loadmat(mat_path)
-
+        new_mat = None
         for index, frame in enumerate(mat.get('data')):
-            first = np.append(frame, frame[: 32])
-            second = np.append(frame[-8:], np.append(frame, frame[: 24]))
-            third = np.append(frame[-16:], np.append(frame, frame[: 16]))
-            four = np.append(frame[-24:], np.append(frame, frame[: 8]))
-            five = np.append(frame[-32:], frame)
-            six = np.append(frame[-40:], frame[: -8])
-            seven = np.append(frame[-48:], frame[: -16])
-            eight = np.append(frame[-56:], frame[: -24])
-            night = np.append(frame[-64:], frame[: -32])
-            ten = np.append(frame[-72:], frame[: -40])
-            eleven = np.append(frame[-80:], frame[: -48])
-            twelve = np.append(frame[-88:], frame[: -56])
-            thirteen = np.append(frame[-96:], frame[: -64])
-            fortheen = np.append(frame[-104:], frame[: -72])
-            fifteen = np.append(frame[-112:], frame[: -80])
-            sixteen = np.append(frame[-120:], frame[: -88])
-
-            whole = np.vstack((first,
-                               np.vstack((
-                                   second, np.vstack((
-                                       third, np.vstack((
-                                           four, np.vstack((
-                                               five, np.vstack((
-                                                   six, np.vstack((
-                                                       seven, np.vstack((
-                                                           eight, np.vstack((
-                                                               night, np.vstack((
-                                                                   ten, np.vstack((
-                                                                       eleven, np.vstack((
-                                                                           twelve, np.vstack((
-                                                                               thirteen, np.vstack((
-                                                                                   fortheen, np.vstack((
-                                                                                       fifteen, sixteen
-                                                                                   ))
-                                                                               ))
-                                                                           ))
-                                                                       ))
-                                                                   ))))
-                                                           ))
-                                                       ))
-                                                   ))
-                                               ))
-                                           ))))))))))
+            if trial_index % 2 != 0:
+                first = np.append(frame, frame[: 32])
+                three = np.append(frame[-16:], np.append(frame, frame[: 16]))
+                five = np.append(frame[-32:], frame)
+                seven = np.append(frame[-48:], frame[: -16])
+                nine = np.append(frame[-64:], frame[: -32])
+                eleven = np.append(frame[-80:], frame[: -48])
+                thirteen = np.append(frame[-96:], frame[: -64])
+                fifteen = np.append(frame[-112:], frame[: -80])
+                whole = np.vstack((
+                    first, np.vstack((
+                        three, np.vstack((
+                            five, np.vstack((
+                                seven, np.vstack((
+                                    nine, np.vstack((
+                                        eleven, np.vstack((
+                                            thirteen, fifteen
+                                        ))
+                                    ))
+                                ))
+                            ))
+                        ))
+                    ))
+                ))
+            else:
+                two = np.append(frame[-8:], np.append(frame, frame[: 24]))
+                four = np.append(frame[-24:], np.append(frame, frame[: 8]))
+                six = np.append(frame[-40:], frame[: -8])
+                eight = np.append(frame[-56:], frame[: -24])
+                ten = np.append(frame[-72:], frame[: -40])
+                twelve = np.append(frame[-88:], frame[: -56])
+                fortheen = np.append(frame[-104:], frame[: -72])
+                sixteen = np.append(frame[-120:], frame[: -88])
+                whole = np.vstack((
+                    two, np.vstack((
+                        four, np.vstack((
+                            six, np.vstack((
+                                eight, np.vstack((
+                                    ten, np.vstack((
+                                        twelve, np.vstack((
+                                            fortheen, sixteen
+                                        ))
+                                    ))
+                                ))
+                            ))
+                        ))
+                    ))
+                ))
             if index == 0:
                 new_mat = whole
             else:
                 new_mat = np.vstack((new_mat, whole))
+
         mat.__setitem__('data', new_mat)
         save_path = os.path.join('.cache2', 'dba', 'data', mat_name)
         scipy.io.savemat(save_path, mat)
---
+++ record.md
@@ -0,0 +1,9 @@
+# Record
+
+## 2018.01.29
+
+add four rows to each trial
+
+* Single Frame accuracy: 0.892378
+* 150 Frames (150ms) majority voting accuracy: 0.995905
+* Per-trial majority voting accuracy: 0.998611
