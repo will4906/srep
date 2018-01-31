f0d639acd3637cb0a4c61d4073149734f8267ae0
will4906 <553105821@qq.com>
2018-02-01 00:42:52 +0800

    add keras cnn learning rate

--- kks.py
+++ kks.py
@@ -14,13 +14,14 @@ if __name__ == '__main__':
     if os.path.exists('save_single') is False:
         os.mkdir('save_single')
 
-    model = load_model('srep_all.h5')
+    if os.path.exists('save_single' + os.sep + 'srep_' + sys.argv[1] + '.h5') is False:
+        model = load_model('srep_all.h5')
 
-    train_x, train_y, test_x, test_y = load_single_train_data('.cache2/dba/data', int(sys.argv[1]))
-    train_x = train_x.reshape(train_x.shape[0], 16, 8)
-    test_x = test_x.reshape(train_x.shape[0], 16, 8)
-    train_y = keras.utils.to_categorical(train_y - 1, 8)
-    test_y = keras.utils.to_categorical(test_y - 1, 8)
+        train_x, train_y, test_x, test_y = load_single_train_data('.cache2/dba/data', int(sys.argv[1]))
+        train_x = train_x.reshape(train_x.shape[0], 16, 8)
+        test_x = test_x.reshape(train_x.shape[0], 16, 8)
+        train_y = keras.utils.to_categorical(train_y - 1, 8)
+        test_y = keras.utils.to_categorical(test_y - 1, 8)
 
-    model.fit(train_x, train_y, batch_size=1000, validation_data=(test_x, test_y), epochs=100)
-    model.save('save_single' + os.sep + 'srep_' + sys.argv[1] + '.h5')
+        model.fit(train_x, train_y, batch_size=1000, validation_data=(test_x, test_y), epochs=100)
+        model.save('save_single' + os.sep + 'srep_' + sys.argv[1] + '.h5')
